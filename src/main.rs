0836a37d54119d01d3c43616daf21b1d9881d625
usari14 <udogar583@gmail.com>
2026-06-07 09:59:16 -0700

    deploy

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use actix_files::Files;
-use actix_web::{App, HttpResponse, HttpServer, web};
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server running at http://localhost:8080");
     HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-            .service(Files::new("/assets", "static").prefer_utf8(true))
+        App::new().service(
+            Files::new("/", "static")
+                .index_file("index.html")
+                .prefer_utf8(true),
+        )
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
-}
-
-async fn index() -> HttpResponse {
-    let html = std::fs::read_to_string("static/index.html").unwrap();
-    HttpResponse::Ok().content_type("text/html").body(html)
 }
